473ba616344c65ae8095cb582ceb87889203824e
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-19 22:44:42 +0200

    updated information

--- Lesson 17/models.py
+++ Lesson 17/models.py
@@ -110,11 +110,10 @@ class Candidate():
         self.main_skill = main_skill
         self.main_skill_grade = main_skill_grade
 
+
 class Vacancy():
 
     def __init__(self, title, main_skill, main_skill_level):
-        self.title=title
+        self.title = title
         self.main_skill = main_skill
         self.main_skill_level = main_skill_level
-
-
