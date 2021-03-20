f51355c0693a44cc9047ddb43115229dd345e2bf
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-20 13:15:20 +0200

    fixed bags

--- Lesson 17/app.py
+++ Lesson 17/app.py
@@ -1,6 +1,7 @@
 import models as mod
 
-if __name__ == '__main__':
+
+def main():
     ivan = mod.Recruiter("Ivan", 100, 23)
     print(ivan)
     alex = mod.Programmer("Alex", 200, 23, {'knowledge of English ', 'Python', 'JS', 'Git', 'html&css'})
@@ -11,3 +12,7 @@ if __name__ == '__main__':
     petr = mod.Candidate('Petr', 'petos@gmail.com', {'Java', 'Git', 'Postgresql'}, 'Java', 'Senior')
     python_vacancy = mod.Vacancy('Vacancy Python', 'Python', 'Middle')
     java_vacancy = mod.Vacancy('Vacancy Java', 'Java', 'Senior')
+
+
+if __name__ == '__main__':
+    main()
