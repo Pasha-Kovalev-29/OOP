9c36668ee50f17e08872b4eb25a28281d12186cf
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-26 21:18:26 +0200

    add tast 3

---
+++ Lesson 19/app.py
@@ -0,0 +1,35 @@
+import models as mod
+import logging
+import datetime
+
+logging.basicConfig(filename="logs.py", datefmt=datetime.datetime.today(), level=logging.INFO)
+
+
+def main():
+    # try:
+    ivan = mod.Recruiter("Ivan", 'ivan@gmail.com', 100, 23)
+    print(ivan)
+    print(ivan.cheak_salary())
+    alex = mod.Programmer("Alex", 'alex@gmail.com', 200, 23,
+                          {'knowledge of English ', 'Python', 'JS', 'Git', 'html&css'})
+    print(alex)
+    alesha = mod.Programmer("Alesha", 'alexsei@gmail.com', 200, 23,
+                            {'knowledge of English ', 'Python', 'Flask', 'Django'})
+    print(alesha)
+    vitalik = mod.Candidate('Vitalik', 'vitos@gmail.com', {'Python', 'Git', 'Postgresql'}, 'Python', 'Middle')
+    petr = mod.Candidate('Petr', 'petos@gmail.com', {'Java', 'Git', 'Postgresql'}, 'Java', 'Senior')
+    python_vacancy = mod.Vacancy('Vacancy Python', 'Python', 'Middle')
+    java_vacancy = mod.Vacancy('Vacancy Java', 'Java', 'Senior')
+    alfa = alex + alesha
+    print(alfa)
+    print(alex.full_info)
+    mod.Candidate.create_candidates("candidates.csv")
+
+    # except Exception as e:
+    #     with open('logs.py', 'a+') as fh:
+    #         fh.write(str(datetime.datetime.today()) + ' ')
+    #     logging.exception(e)
+
+
+if __name__ == '__main__':
+    main()
